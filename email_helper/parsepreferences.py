d8dc880954b539757aa659375a017436ef062c41
MaxDLink <max.durham.link@gmail.com>
2023-05-11 14:34:19 -0500

    changed variables

--- email_helper/parsepreferences.py
+++ email_helper/parsepreferences.py
@@ -1,16 +1,16 @@
 import json
 
 #parses the preferences .json file
-def parse_preferences(file_path): 
-    with open(file_path, 'r') as file:
-        data = json.load(file)
+def parse_preferences(preferences): 
+    with open(preferences, 'r') as file:
+        data = json.load(preferences)
     preferences = data['ai_email_bot']['student preferences']
     return preferences
 
 #asks the user to adjust the preferences based on what they want 
-def prompt_for_preferences(preferences):
+def prompt_for_preferences(file):
     # Get current depth level
-    current_depth = preferences["ai_email_bot"]["user preferences"]["depth"]
+    current_depth = file["ai_email_bot"]["user preferences"]["depth"]
     print(f"Current depth level is: {current_depth}")
 
     while True:
@@ -19,7 +19,7 @@ def prompt_for_preferences(preferences):
             if new_depth not in [1, 2, 3]:
                 print("Invalid depth level. Please enter a number between 1 and 3.")
                 continue
-            preferences["ai_email_bot"]["user preferences"]["depth"] = new_depth
+            file["ai_email_bot"]["user preferences"]["depth"] = new_depth
             break
         except ValueError:
             print("Invalid input. Please enter a number.")
@@ -29,46 +29,46 @@ def prompt_for_preferences(preferences):
     valid_tone_styles = ["Debate", "Encouraging", "Neutral", "Informative", "Friendly"]
 
     # Get current communication styles
-    current_comm_styles = preferences["ai_email_bot"]["user preferences"]["communication_style"]
+    current_comm_styles = file["ai_email_bot"]["user preferences"]["communication_style"]
     print(f"Current communication styles are: {current_comm_styles}")
 
     while True:
         new_comm_styles = input("Please enter new communication styles (separated by commas): ").split(',')
         if all(style in valid_comm_styles for style in new_comm_styles):
-            preferences["ai_email_bot"]["user preferences"]["communication_style"] = new_comm_styles
+            file["ai_email_bot"]["user preferences"]["communication_style"] = new_comm_styles
             break
         else:
             print("Invalid communication style. Please enter one or more of the following (separated by commas): " + ', '.join(valid_comm_styles))
 
     # Get current tone styles
-    current_tone_styles = preferences["ai_email_bot"]["user preferences"]["tone_style"]
+    current_tone_styles = file["ai_email_bot"]["user preferences"]["tone_style"]
     print(f"Current tone styles are: {current_tone_styles}")
 
     while True:
         new_tone_styles = input("Please enter new tone styles (separated by commas): ").split(',')
         if all(style in valid_tone_styles for style in new_tone_styles):
-            preferences["ai_email_bot"]["user preferences"]["tone_style"] = new_tone_styles
+            file["ai_email_bot"]["user preferences"]["tone_style"] = new_tone_styles
             break
         else:
             print("Invalid tone style. Please enter one or more of the following (separated by commas): " + ', '.join(valid_tone_styles))
 
     # Get current emoji usage
-    current_emoji_usage = preferences["ai_email_bot"]["user preferences"]["use_emojis"]
+    current_emoji_usage = file["ai_email_bot"]["user preferences"]["use_emojis"]
     print(f"Current emoji usage is: {'Yes' if current_emoji_usage else 'No'}")
 
     while True:
         new_emoji_usage = input("Do you want to use emojis? (Yes/No): ")
         if new_emoji_usage.lower() in ['yes', 'no']:
-            preferences["ai_email_bot"]["user preferences"]["use_emojis"] = new_emoji_usage.lower() == 'yes'
+            file["ai_email_bot"]["user preferences"]["use_emojis"] = new_emoji_usage.lower() == 'yes'
             break
         else:
             print("Invalid input. Please enter 'Yes' or 'No'.")
 
     # Get current language
-    current_language = preferences["ai_email_bot"]["user preferences"]["language"]
+    current_language = file["ai_email_bot"]["user preferences"]["language"]
     print(f"Current language is: {current_language}")
 
     new_language = input("Please enter a new language: ")
-    preferences["ai_email_bot"]["user preferences"]["language"] = new_language
+    file["ai_email_bot"]["user preferences"]["language"] = new_language
 
-    return preferences
+    return file
